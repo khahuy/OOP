09004287296973c33ed8803e40d4e611624802b4
phandangsang <phandangsang05@gmail.com>
2025-10-16 19:38:48 +0700

    them interface

---
+++ ICHUCNANG.java
@@ -0,0 +1,11 @@
+public interface ICHUCNANG {
+    void them();
+
+    void sua();
+
+    void xoa();
+
+    void hienThi();
+
+    void timKiem();
+}
---
+++ IFILE.java
@@ -0,0 +1,5 @@
+public interface IFILE {
+    void ghiFile();
+
+    void docFile();
+}
